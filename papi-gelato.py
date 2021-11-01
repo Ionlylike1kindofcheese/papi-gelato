4808a806a26737b592272ec88516edfffc9e54e1
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-11-01 10:27:01 +0100

    dit hoort bij opdracht: F1.5.02.O3 - 3 nieuwe smaken (Check versiegeschiedenis voor vorige commits)

--- papi-gelato.py
+++ papi-gelato.py
@@ -5,6 +5,8 @@ def showNotunderstood():
 programcompletion = False
 phase1 = False
 phase2 = False
+flavourquestions = False
+bolnummer = 1
 
 print("Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is.")
 
@@ -39,9 +41,28 @@ while programcompletion == False:
             print("Sorry zulke grote bakken hebben we niet")
         else:
             showNotunderstood()
+        
+    while flavourquestions == False:
+        if bolnummer <= int(aantalbollen):
+            askflavour = input("Welke smaak wilt u voor bolletje nummer " + str(bolnummer) + "? A) Aardbei, C) Chocolade, M) Munt of V) Vanille? ")
+            if askflavour == "A":
+                flavour = "aardbei"
+            elif askflavour == "C":
+                flavour = "chocolade"
+            elif askflavour == "M":
+                flavour = "munt"
+            elif askflavour == "V":
+                flavour = "vanille"
+            else:
+                showNotunderstood()
+            
+            if askflavour == "A" or askflavour == "C" or askflavour == "M" or askflavour == "V":
+                bolnummer = bolnummer + 1
+        else:
+            flavourquestions = True
 
     while phase2 == False:
-        yesorno = str(input("Hier is uw " + hoornofbak + " met " + str(aantalbollen) + enkelofmeerderebollen + ". Wilt u nog meer bestellen? (Y/N)"))
+        yesorno = str(input("Hier is uw " + hoornofbak + " met " + str(aantalbollen) + enkelofmeerderebollen + ". Wilt u nog meer bestellen? (Y/N) "))
         if yesorno == "Y":
             print("")
             phase2 = True
