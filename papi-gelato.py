832b9de48a167f092a8e0d9b7bc7cfca822669a8
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-11-01 13:09:19 +0100

    Dit hoort bij opdracht: F1.5.02.O5 - Feature: Een bonnetje (Check versiegeschiedenis voor vorige commits)

--- papi-gelato.py
+++ papi-gelato.py
@@ -5,37 +5,50 @@ def showNotunderstood():
 programcompletion = False
 phase1 = False
 phase2 = False
+phase3 = False
 flavourquestions = False
 bolnummer = 1
+totaalaantalbollen = 0
+aantalhoorntjes = 0
+aantalbakjes = 0
+prijsbollen = 0
+prijshoorntjes = 0
+prijsbakjes = 0
 
 print("Welkom bij Papi Gelato")
 
 while programcompletion == False:
     while phase1 == False:
         aantalbollen = int(input("Hoeveel bolletjes wilt u? "))
+        totaalaantalbollen = totaalaantalbollen + aantalbollen
         if aantalbollen == 1:
             enkelofmeerderebollen = " bolletje"
         elif aantalbollen >= 2:
             enkelofmeerderebollen = " bolletjes"
         
         if aantalbollen >= 1 and aantalbollen <= 3:
-            askHoornofbak = str(input("Wilt u deze " + str(aantalbollen) + enkelofmeerderebollen + " in A) een hoorntje of B) een bakje? "))
-            if askHoornofbak == "A":
-                hoornofbak = "hoorntje"
-            elif askHoornofbak == "B":
-                hoornofbak = "bakje"
-            else:
-                showNotunderstood()
-            
-            if askHoornofbak == "A" or hoornofbak == "B":
-                phase1 = True
-                print("")
-            else:
-                print("")
+            phase1 = True
+            while phase3 == False:
+                askHoornofbak = str(input("Wilt u deze " + str(aantalbollen) + enkelofmeerderebollen + " in A) een hoorntje of B) een bakje? "))
+                if askHoornofbak == "A":
+                    hoornofbak = "hoorntje"
+                    aantalhoorntjes = aantalhoorntjes + 1
+                elif askHoornofbak == "B":
+                    hoornofbak = "bakje"
+                    aantalbakjes = aantalbakjes + 1
+                else:
+                    showNotunderstood()
+                
+                if askHoornofbak == "A" or askHoornofbak == "B":
+                    phase3 = True
+                    print("")
+                else:
+                    print("")
         
         elif aantalbollen >= 4 and aantalbollen <= 8:
             print("Dan krijgt u van mij een bakje met " + str(aantalbollen) + str(enkelofmeerderebollen))
-            hoornofbak = "hoorntje"
+            hoornofbak = "bakje"
+            aantalbakjes = aantalbakjes + 1
             phase1 = True
         elif aantalbollen > 8:
             print("Sorry zulke grote bakken hebben we niet")
@@ -71,3 +84,22 @@ while programcompletion == False:
             phase2 = True
         else:
             showNotunderstood()
+    if programcompletion == False:
+        phase1 = False
+        phase2 = False
+
+
+if programcompletion == True:
+    prijsbollen = totaalaantalbollen * 1.10
+    prijshoorntjes = aantalhoorntjes * 1.25
+    prijsbakjes = aantalbakjes * 0.75
+    print('---------["Papi Gelatto"]---------')
+    print("")
+    print("Bolletje     " + str(totaalaantalbollen) + " x €1,10 = €" + str(prijsbollen))
+    if aantalhoorntjes >= 1:
+        print("horrentje    " + str(aantalhoorntjes) + " x €1,25 = €" + str(prijshoorntjes))
+    if aantalbakjes >= 1:
+        print("bakje        " + str(aantalbakjes) + " x €0,75 = €" + str(prijsbakjes))
+    print("                        -------- +")
+    totaalprijs = prijsbollen + prijshoorntjes + prijsbakjes
+    print("totaal                 = €" + str(totaalprijs))
